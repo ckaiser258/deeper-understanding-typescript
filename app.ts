e058c5df4ceb1de10180b8dfeb1156ea7512eeba
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-29 12:04:21 -0600

    Add examples and information on tuples.

--- app.ts
+++ app.ts
@@ -4,7 +4,23 @@
 //   age: number;
 // }
 //when this is made:
-const person = { name: "Colton", age: 28, hobbies: ["Sports", "Cooking"] };
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string]; //this is a tuple. typescript knows this property needs to have exactly this structure
+} = {
+  name: "Colton",
+  age: 28,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
+};
+
+person.role.push("admin"); //this does NOT cause an error, an unfortunate problem in typescript
+//person.role[1] = 10 //error
+
+//person.role = [0, 'admin', 'user']
+//THIS causes an error however (which we want) whereas pushing does not
 
 //remember, typescript can infer that this is of type string[] just from assigning it to an array of strings
 //explicitly telling it the type like below is used often for when the variable might be changed
