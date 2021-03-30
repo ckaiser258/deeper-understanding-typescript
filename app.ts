8ac9773ec0ef71dfdd627176aa75e99cb7918561
Colton Kaiser <kaiserc2@seattleu.edu>
2021-03-30 13:58:21 -0600

    Add notes and functionality on enums.

--- app.ts
+++ app.ts
@@ -4,19 +4,35 @@
 //   age: number;
 // }
 //when this is made:
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string]; //this is a tuple. typescript knows this property needs to have exactly this structure
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; //this is a tuple. typescript knows this property needs to have exactly this structure
+// } = {
+//   name: "Colton",
+//   age: 28,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+//Below gives ADMIN a value of 5, READ_ONLY a value of 6, and AUTHOR a value of 7
+//can customize each as well with whatever you want (even strings etc.)
+//useful when you want constants assigned that might be hard to remember down the road
+enum Role {
+  ADMIN = 5,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
   name: "Colton",
   age: 28,
   hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  role: Role.ADMIN,
 };
 
-person.role.push("admin"); //this does NOT cause an error, an unfortunate problem in typescript
+// person.role.push("admin"); //this does NOT cause an error if role is a tuple, an unfortunate problem in typescript
 //person.role[1] = 10 //error
 
 //person.role = [0, 'admin', 'user']
@@ -32,4 +48,8 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase()); //since typescript knows hobby will be a string, we get intellisense for any string ahead of time
   //hobby.map() throws an error ahead of time!
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is an admin");
 }
