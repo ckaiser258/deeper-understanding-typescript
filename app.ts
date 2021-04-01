a0207860403b77c86ff391e9306bf716cc3b00de
Colton Kaiser <kaiserc2@seattleu.edu>
2021-04-01 14:45:35 -0600

    Type aliases.

--- app.ts
+++ app.ts
@@ -1,10 +1,15 @@
-//union types are helpful when something can take more than one type of values
+//type aliases (custom types):
+type Combinable = number | string; //union types are helpful when something can take more than one type of values
+//below is a union type combined with a literal type. resultConversion needs to be one of these two options
+//this prevents us from accidentally typing in the wrong thing
+type ConversionDescriptor = "as-number" | "as-text";
+
 function combine(
-  input1: number | string,
-  input2: number | string,
+  input1: Combinable,
+  input2: Combinable,
   //below is a union type combined with a literal type. resultConversion needs to be one of these two options
   //this prevents us from accidentally typing in the wrong thing
-  resultConversion: "as-number" | "as-text"
+  resultConversion: ConversionDescriptor
 ) {
   let result;
 
