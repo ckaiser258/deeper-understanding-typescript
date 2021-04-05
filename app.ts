1b5fa7cade1c201642f25550158dc5f92427dab3
Colton Kaiser <kaiserc2@seattleu.edu>
2021-04-05 09:42:16 -0700

    The never type.

--- app.ts
+++ app.ts
@@ -9,3 +9,13 @@ if (typeof userInput === "string") {
   //when using 'any', that wouldn't break
   userName = userInput;
 }
+
+function generateError(message: string, code: number): never {
+  //since this function will NEVER return anything (only returns an error and crashes the script)
+  //it should be set to the never type as opposed to void
+  throw { message: message, errorCode: code };
+}
+
+const result = generateError("An error occured", 500);
+
+console.log(result);
