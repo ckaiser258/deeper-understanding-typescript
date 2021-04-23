2c056c9cc96e0f2bdf2b61d17ca677acc20b1da0
Colton Kaiser <kaiserc2@seattleu.edu>
2021-04-23 14:15:08 -0600

    Add and implement getter and setter methods.

--- src/app.ts
+++ src/app.ts
@@ -40,12 +40,33 @@ class ITDepartment extends Department {
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
+
+  //getters are methods that allow us to access private properties outside the class in ways defined
+  get mostRecentReport() {
+    if (this.lastReport) {
+      //getters need to return something
+      return this.lastReport;
+    }
+    throw new Error("No report found.");
+  }
+
+  //setters allow us to modify private properties outside the class in ways defined
+  set mostRecentReport(value: string) {
+    if (!value) {
+      throw new Error("Please pass in a valid value.");
+    }
+    this.addReport(value);
+  }
+
   constructor(id: string, private reports: string[]) {
     super(id, "Accounting");
+    this.lastReport = reports[0];
   }
   //Can add methods for inherited classes as well
   addReport(text: string) {
     this.reports.push(text);
+    this.lastReport = text;
   }
   //Can override methods and properties defined in the class being inherited from
   addEmployee(name: string) {
@@ -71,7 +92,14 @@ console.log(it);
 
 const accounting = new AccountingDepartment("d2", []);
 
+//this calls the mostRecent report setter method defined in AccountingDepartment
+//notice it does not need to be called with ()
+accounting.mostRecentReport = "Year End Report";
 accounting.addReport("Something went wrong...");
+
+//this calls the mostRecent report getter method defined in AccountingDepartment
+//notice it does not need to be called with ()
+console.log(accounting.mostRecentReport);
 
 accounting.addEmployee("Colton");
 accounting.addEmployee("Max");
