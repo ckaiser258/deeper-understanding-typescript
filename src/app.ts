ca93b523af0ceddb7745f883a3c56535ab118b7d
Colton Kaiser <kaiserc2@seattleu.edu>
2021-04-29 12:32:04 -0600

    Create class based on interface and add notes.

--- src/app.ts
+++ src/app.ts
@@ -2,21 +2,33 @@
 //different from type because interfaces can only be used to describe the structure of an object
 //interfaces are clearer because of this for defining object types
 //can implement an interface within a class
-interface Person {
+//interfaces are often used to share structure amongst different classes
+//different from abstract classes because abstract classes have concrete implementation and overriding
+interface Greetable {
   name: string;
-  age: number;
 
   greet(phrase: string): void;
 }
 
-let user1: Person;
+//can implement multiple interfaces (separated by comma) as opposed to extends where you can only inherit one
+//can create more methods and properties within this class (and even use it as extends) but now it needs to include the Greetable structure
+class Person implements Greetable {
+  name: string;
+  age = 30;
 
-user1 = {
-  name: "Colton",
-  age: 30,
+  constructor(n: string) {
+    this.name = n;
+  }
+
   greet(phrase: string) {
     console.log(`${phrase} ${this.name}`);
-  },
-};
+  }
+}
+
+//can also say user1: Person. but this works too because Person is based on the Greetable interface
+let user1: Greetable;
+
+user1 = new Person("Colton");
 
 user1.greet("Hi there - I am");
+console.log(user1);
