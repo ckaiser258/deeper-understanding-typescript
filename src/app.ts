26f699d901b70fc7123ad15f50b134719c894a8e
Colton Kaiser <kaiserc2@seattleu.edu>
2021-05-04 10:59:37 -0600

    Add examples of type guards and notes.

--- src/app.ts
+++ src/app.ts
@@ -22,3 +22,59 @@ type Numeric = number | boolean;
 //in union types, the types they have in common are the intersection type
 //ex: Universal is of type number because it is the only thing the two have in common, but could be more
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  //type guarding:
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  //type guarding with 'in':
+  console.log(`Name: ${emp.name}`);
+  if ("privileges" in emp) {
+    console.log(`Privileges: ${emp.privileges}`);
+  }
+  if ("startDate" in emp) {
+    console.log(`Start Date: ${emp.startDate}`);
+  }
+}
+
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log(`Loading cargo ... ${amount}`);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+  //type guarding with instanceof
+  //this only works with classes because javascript supports this. it doesn't support interfaces (they aren't actually compiled)
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
