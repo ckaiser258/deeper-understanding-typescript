27947cade296aaa89fce5b9a8686c038a73b3c9f
Colton Kaiser <kaiserc2@seattleu.edu>
2021-05-11 11:32:30 -0600

    Create generic function and notes.

--- src/app.ts
+++ src/app.ts
@@ -15,3 +15,15 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then((data) => {
   data.split(" ");
 });
+
+// create our own generic as a function
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Colton", hobbies: ["Sports"] }, { age: 28 });
+//since we've made merge() a generic function, typescript knows it will return the two specific objects passed into it
+//and won't complain about grabbing a property that exists on the new object being returned
+// if we did merge(objA: object, objB: object), typescript wouldn't let us access "age" on this because
+//all it would know is that two random objects are being returned
+console.log(mergedObj.age);
