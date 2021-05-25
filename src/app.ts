930da0f5e24a6286a8b7ceda75ddfcdc9790bb0a
Colton Kaiser <kaiserc2@seattleu.edu>
2021-05-25 13:21:21 -0600

    Add decorator that renders h1 text to dom.

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,20 @@ function Logger(logString: string) {
     }
 }
 
+function WithTemplate(template: string, hookId: string) {
+    return function(constructor: any) {
+        const hookEl = document.getElementById(hookId)
+        const p = new constructor()
+        if (hookEl) {
+            hookEl.innerHTML = template
+            hookEl.querySelector('h1')!.textContent = p.name
+        }
+    }
+}
+
 // decorators execute when you define an object, not when you create it (with new Person())
-@Logger('LOGGING - PERSON')
+// @Logger('LOGGING - PERSON')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = "Colton"
 
