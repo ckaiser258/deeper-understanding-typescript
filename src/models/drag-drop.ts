ab0b531986d33d62925c9b4ffe92cee1b28884fb
Colton Kaiser <kaiserc2@seattleu.edu>
2021-07-30 13:35:34 -0600

    Abstract to namespaces and organize files and folders

--- src/models/drag-drop.ts
+++ src/models/drag-drop.ts
@@ -1,10 +1,10 @@
 // Drag & Drop Interfaces
 namespace App {
-    export interface Draggable {
+  export interface Draggable {
     dragStartHandler(event: DragEvent): void;
     dragEndHandler(event: DragEvent): void;
   }
-  
+
   export interface DragTarget {
     dragOverHandler(event: DragEvent): void;
     dropHandler(event: DragEvent): void;
