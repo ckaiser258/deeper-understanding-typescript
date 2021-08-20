4c2e908ec4907e185e51c931a97f29b2215ee72a
Colton Kaiser <kaiserc2@seattleu.edu>
2021-08-20 14:26:25 -0600

    Use 'declare' and add notes.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import _ from "lodash";
 
+// typescript doesn't know that we've added a GLOBAL var to index.html
+// so we use 'declare' to make it work
+// this can be done with any js packages that don't have a types package
+declare var GLOBAL: any;
+
 console.log(_.shuffle([1, 2, 3]));
+
+console.log(GLOBAL);
