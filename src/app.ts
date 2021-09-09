88a82188ffab369d632e1aa115bfeedc2153d5e6
Colton Kaiser <kaiserc2@seattleu.edu>
2021-09-09 13:38:07 -0600

    Create axios get request.

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,35 @@
+import axios from "axios";
+
 const form = document.querySelector("form");
 const addressInput = document.getElementById("address") as HTMLInputElement;
 
 const googleApiKey = process.env.GOOGLE_API_KEY;
 
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
 function searchAddressHandler(e: Event) {
   e.preventDefault();
   const enteredAddress = addressInput?.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${googleApiKey}`
+    )
+    .then((res) => {
+      if (res.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coordinates = res.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
 form?.addEventListener("submit", searchAddressHandler);
